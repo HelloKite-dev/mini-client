93b4ca3c0b2451ea42984ebd1f7a621a254221a0
HelloKite-dev <sy05200801@gmail.com>
2026-03-17 10:45:12 +0900

    라우터 설정

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,42 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/member',
+      name: 'member',
+      component: () => import('../views/MemberView.vue')
+    },
+    {
+      path: '/posts',
+      name: 'postList',
+      component: () => import('../views/PostListView.vue')
+    },
+    {
+      path: '/posts/:id',
+      name: 'postDetail',
+      component: () => import('../views/PostDetailView.vue')
+    },
+    {
+      path: '/posts/form',
+      name: 'postForm',
+      component: () => import('../views/PostFormView.vue')
+    },
+    {
+      path: '/mypage',
+      name: 'mypage',
+      component: () => import('../views/MypageView.vue')
+    },
+    {
+      path: '/',
+      redirect: '/login'
+    }
+  ],
 })
 
 export default router
