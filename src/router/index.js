4a4fde7c8767d66db1e40d9c010f91612e26c747
HelloKite-dev <sy05200801@gmail.com>
2026-03-31 14:30:10 +0900

    페이지 경로 추가

--- src/router/index.js
+++ src/router/index.js
@@ -6,37 +6,42 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginView.vue')
+      component: () => import('../views/LoginView.vue'),
     },
     {
       path: '/member',
       name: 'member',
-      component: () => import('../views/MemberView.vue')
+      component: () => import('../views/MemberView.vue'),
     },
     {
       path: '/posts',
       name: 'postList',
-      component: () => import('../views/PostListView.vue')
-    },
-    {
-      path: '/posts/:id',
-      name: 'postDetail',
-      component: () => import('../views/PostDetailView.vue')
+      component: () => import('../views/PostListView.vue'),
     },
     {
       path: '/posts/form',
       name: 'postForm',
-      component: () => import('../views/PostFormView.vue')
+      component: () => import('../views/PostFormView.vue'),
     },
     {
-      path: '/mypage',
+      path: '/posts/:id',
+      name: 'postDetail',
+      component: () => import('../views/PostDetailView.vue'),
+    },
+    {
+      path: '/mypage/:userId',
       name: 'mypage',
-      component: () => import('../views/MypageView.vue')
+      component: () => import('../views/MypageView.vue'),
+    },
+    {
+      path: '/password',
+      name: 'password',
+      component: () => import('../views/PasswordView.vue'),
     },
     {
       path: '/',
-      redirect: '/login'
-    }
+      redirect: '/login',
+    },
   ],
 })
 
