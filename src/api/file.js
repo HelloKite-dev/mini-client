a280f56f0c42e0567c2b06c0390d8e2adfdb1f6f
HelloKite-dev <sy05200801@gmail.com>
2026-03-31 14:30:54 +0900

    파일 삭제 api 추가

--- src/api/file.js
+++ src/api/file.js
@@ -1,21 +1,28 @@
 import api from './index.js'
 
 // 파일 다운로드
-export const downFileApi = async (postId) => {
-	return await api.get(`/api/files/${postId}`)
+export const downFileApi = async (fileId) => {
+  return await api.get(`/api/files/${fileId}`, {
+    responseType: 'blob',
+  })
 }
 
 // 파일 업로드
-export const uploadFileApi= async (postId, file) => {
-	const formData = new FormData()
-	formData.append('postId', postId)
-	formData.append('file', file)
-	return await api.post('/api/files', formData, {
-		headers: { 'Content-Type': 'multipart/form-data' }
-	})
+export const uploadFileApi = async (postId, file) => {
+  const formData = new FormData()
+  formData.append('postId', postId)
+  formData.append('file', file)
+  return await api.post('/api/files', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  })
 }
 
 // 파일 목록 조회
 export const getFileListApi = async (postId) => {
-	return await api.get(`/api/files/post/${postId}`)
+  return await api.get(`/api/files/post/${postId}`)
+}
+
+// 파일 삭제
+export const deleteFileApi = async (fileId) => {
+  return await api.delete(`/api/files/${fileId}`)
 }
